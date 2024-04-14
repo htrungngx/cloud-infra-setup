87aa3ff080671dceac14240da4dfbcad33ab088d
Trung Hoang Nguyen <83159640+htrungngx@users.noreply.github.com>
2024-04-14 14:48:06 +0300

    Delete examples/vm_bootstrap directory

--- examples/vm_bootstrap/main.tf
+++
@@ -1,4 +0,0 @@
-# locals {
-#     bootstrap_script    = try(var.bootstrap_script, file(var.bootstrap_script_file))
-    
-# }
--- examples/vm_bootstrap/variables.tf
+++
@@ -1,9 +0,0 @@
-# variable "bootstrap_script_file" {
-#   type    = string
-#   default = ""
-# }
-
-# variable "bootstrap_script" {
-#   type    = string
-#   default = ""
-# }
